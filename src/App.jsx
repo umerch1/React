56b8fc02bfcee85ee5cdd3e7dcc7c6bcb0ede49a
umerch1 <umerf3024@gmail.com>
2024-10-01 15:56:50 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <h1 className="5xl font-bold underline text-red-600">
+      Hello world!
+    </h1>
+    </>
+    
+  )
+}
+
+export default App
